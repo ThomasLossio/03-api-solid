28fa64b0af3e5c3dceeb2a5cf02ebf4c60d359f2
Thomas Lossio <thomaslossio@hotmail.com>
2024-12-08 14:03:34 -0300

    Create and adjust use case factories

--- src/use-cases/factories/make-get-user-profile-use-case.ts
+++ src/use-cases/factories/make-get-user-profile-use-case.ts
@@ -1,10 +1,10 @@
 import { PrismaUsersRepository }
   from '@/repositories/prisma/prisma-users-repository'
-import { RegisterUseCase } from '../register'
+import { GetProfileUseCase } from '../get-user-profile'
 
-export function makeRegisterUseCase() {
+export function makeGetUserProfileUseCase() {
   const usersRepository = new PrismaUsersRepository()
-  const useCase = new RegisterUseCase(usersRepository)
+  const useCase = new GetProfileUseCase(usersRepository)
 
   return useCase
 }
