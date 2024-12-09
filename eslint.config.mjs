2a0a9208c7f1e2f7cb38b3ee6ba258e624bdc138
Thomas Lossio <thomaslossio@hotmail.com>
2024-12-09 19:07:37 -0300

    Fix eslint ignore no more supported

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,14 @@ export default [
       camelcase: 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
-      '@stylistic/max-len': ['error', { code: 120 }],
+      '@stylistic/max-len': ['warn', {
+        code: 120,
+        tabWidth: 2,
+        ignoreUrls: true,
+        ignoreComments: false,
+      }],
+
     },
+    ignores: ['node_modules', 'build'],
   },
 ]
