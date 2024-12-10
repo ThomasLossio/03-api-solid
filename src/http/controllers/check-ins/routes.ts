eac468884e251c1985091e6d8191ca37b3aaac2a
Thomas Lossio <thomaslossio@hotmail.com>
2024-12-10 20:49:05 -0300

    Check-ins e2e tests finalized

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { metrics } from './metrics'
 import { history } from './history'
+import { validate } from './validate'
 
 export async function checkInRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -12,5 +13,5 @@ export async function checkInRoutes(app: FastifyInstance) {
   app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkInId/validate', create)
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
