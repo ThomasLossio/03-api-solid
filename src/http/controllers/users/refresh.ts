8943b4b5b5465468f378d36e336106176d03d5b3
Thomas Lossio <thomaslossio@hotmail.com>
2024-12-11 20:46:11 -0300

    Authorization by roles (RBAC)

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -5,13 +5,19 @@ export async function refresh(
 ) {
   await request.jwtVerify({ onlyCookie: true })
 
-  const token = await reply.jwtSign({}, {
+  const { role } = request.user
+
+  const token = await reply.jwtSign({
+    role,
+  }, {
     sign: {
       sub: request.user.sub,
     },
   })
 
-  const refreshToken = await reply.jwtSign({}, {
+  const refreshToken = await reply.jwtSign({
+    role,
+  }, {
     sign: {
       sub: request.user.sub,
       expiresIn: '7d',
